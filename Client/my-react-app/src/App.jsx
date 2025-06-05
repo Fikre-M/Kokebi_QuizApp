862a5be96e75e63e9d8cb472841f391fdc1192a4
Fikre-M <fikreddu@gmail.com>
2025-06-05 19:32:47 -0400

    Create the first header

--- Client/my-react-app/src/App.jsx
+++ Client/my-react-app/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Components/Heade/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Zodiac App
-       </div>
+      <Header />
     </>
   )
 }
